6430c912d209a5daecd878da84e3cea90dceaa66
David Mojicevic <david.mojicevic@eng.it>
2019-10-20 19:21:32 +0200

    Add Project Service and Project Controller

---
+++ ppmtool/src/main/java/com/david/ppmtool/services/ProjectService.java
@@ -0,0 +1,7 @@
+package com.david.ppmtool.services;
+
+/**
+ * @author David Mojicevic on 20/10/2019.
+ */
+public class ProjectService {
+}
---
+++ ppmtool/src/main/java/com/david/ppmtool/web/ProjectController.java
@@ -0,0 +1,7 @@
+package com.david.ppmtool.web;
+
+/**
+ * @author David Mojicevic on 20/10/2019.
+ */
+public class ProjectController {
+}

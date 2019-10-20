ae34538645990369a67f03a7251ed1a892a942ed
David Mojicevic <david.mojicevic@eng.it>
2019-10-20 11:27:16 +0200

    Add Project Entity Class and Project Repository

---
+++ ppmtool/src/main/java/com/david/ppmtool/domain/Project.java
@@ -0,0 +1,7 @@
+package com.david.ppmtool.domain;
+
+/**
+ * @author David Mojicevic on 18/10/2019.
+ */
+public class Project {
+}
---
+++ ppmtool/src/main/java/com/david/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.david.ppmtool.repositories;
+
+/**
+ * @author David Mojicevic on 20/10/2019.
+ */
+public interface ProjectRepository {
+}

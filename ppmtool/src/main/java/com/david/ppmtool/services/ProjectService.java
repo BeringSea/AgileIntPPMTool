d6503866a0df59096be0884da294a4033b76bfbf
David Mojicevic <david.mojicevic@eng.it>
2019-10-20 19:24:52 +0200

    Add Project Service and Project Controller

--- ppmtool/src/main/java/com/david/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/david/ppmtool/services/ProjectService.java
@@ -1,7 +1,23 @@
 package com.david.ppmtool.services;
 
+import com.david.ppmtool.domain.Project;
+import com.david.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author David Mojicevic on 20/10/2019.
  */
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        // Logic
+
+        return projectRepository.save(project);
+    }
 }
--- ppmtool/src/main/java/com/david/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/david/ppmtool/web/ProjectController.java
@@ -1,7 +1,29 @@
 package com.david.ppmtool.web;
 
+import com.david.ppmtool.domain.Project;
+import com.david.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author David Mojicevic on 20/10/2019.
  */
+@RestController
+@RequestMapping("/api/project")
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
 }

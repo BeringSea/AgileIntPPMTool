b0fc78fa5e760f363939a2ba280a1e4a8f22b9d1
David Mojicevic <david.mojicevic@eng.it>
2019-10-21 17:07:35 +0200

    Add Project and Layout component

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
     render() {
-        return <h1>Welcome to the Dashboard</h1>;
+        return (
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+            </div>
+        );
     }
 }
+
+export default Dashboard;

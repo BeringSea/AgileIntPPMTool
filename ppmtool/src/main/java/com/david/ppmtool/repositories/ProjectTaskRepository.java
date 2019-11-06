a945e24ec527ca981a88ef1e8e6732416f645473
David Mojicevic <david.mojicevic@eng.it>
2019-11-06 18:08:50 +0100

    Get ProjectTask list

--- ppmtool/src/main/java/com/david/ppmtool/repositories/ProjectTaskRepository.java
+++ ppmtool/src/main/java/com/david/ppmtool/repositories/ProjectTaskRepository.java
@@ -4,9 +4,13 @@ import com.david.ppmtool.domain.ProjectTask;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author David Mojicevic on 05/11/2019.
  */
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask,Long> {
+
+    List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 }

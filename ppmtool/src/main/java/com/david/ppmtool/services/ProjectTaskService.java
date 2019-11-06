37aa2f1303c158127f9345f47ebc71003a4f7117
David Mojicevic <david.mojicevic@eng.it>
2019-11-06 16:36:29 +0100

    Add ProjectTask

---
+++ ppmtool/src/main/java/com/david/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,52 @@
+package com.david.ppmtool.services;
+
+import com.david.ppmtool.domain.Backlog;
+import com.david.ppmtool.domain.ProjectTask;
+import com.david.ppmtool.repositories.BacklogRepository;
+import com.david.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author David Mojicevic on 05/11/2019.
+ */
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        // all project tasks to a specific project, not null project, that means that Backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        // set the backlog to the project task
+        projectTask.setBacklog(backlog);
+
+        // set project sequence to bi nice to read
+        Integer backlogSequence = backlog.getPTSequence();
+
+        // update Backlog sequence
+        backlogSequence++;
+        backlog.setPTSequence(backlogSequence);
+
+        // add sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        // set initial status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        // set initial priority when priority is null
+        if (projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        }
+        return projectTaskRepository.save(projectTask);
+    }
+}
